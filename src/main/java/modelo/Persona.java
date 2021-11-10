6eea27349338250d0bc3f559cf3ea10a9ba9ce2a
j-ortiz-ici <j.ortiz07@ufromail.cl>
2021-11-09 23:07:54 -0300

    7to commit

---
+++ src/main/java/modelo/Persona.java
@@ -0,0 +1,12 @@
+package modelo;
+
+import java.util.Date;
+
+public class Persona extends Usuario {
+    protected String run;
+    protected Date nacimiento;
+    protected String nacionalidad;
+    protected String celular;
+    protected String domicilio;
+    public Persona() {}
+}
